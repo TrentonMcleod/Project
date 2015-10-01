64352eb773a63e5b25716006428c9605e65d344e
Trenton Mcleod <tcloud81915@gmail.com>
2015-10-01 19:37:01 -0400

    the containment process Address

--- BusinessProject/Person.cs
+++ BusinessProject/Person.cs
@@ -10,6 +10,8 @@ namespace BusinessProject
     {
         //Fields or properties
         private string _firstName;
+        //Containment
+        private Address address;
         private string _lastName;
         private string _email;
 
