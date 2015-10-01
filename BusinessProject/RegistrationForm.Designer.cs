7c29d711cd10c10baf40e33a35ee145f662f69b9
Trenton Mcleod <tcloud81915@gmail.com>
2015-10-01 18:05:25 -0400

    Made the section object

--- BusinessProject/RegistrationForm.Designer.cs
+++ BusinessProject/RegistrationForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnStudent = new System.Windows.Forms.Button();
             this.btnInstructor = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStudent
@@ -44,7 +45,7 @@
             // 
             // btnInstructor
             // 
-            this.btnInstructor.Location = new System.Drawing.Point(101, 88);
+            this.btnInstructor.Location = new System.Drawing.Point(73, 68);
             this.btnInstructor.Name = "btnInstructor";
             this.btnInstructor.Size = new System.Drawing.Size(75, 23);
             this.btnInstructor.TabIndex = 1;
@@ -52,11 +53,21 @@
             this.btnInstructor.UseVisualStyleBackColor = true;
             this.btnInstructor.Click += new System.EventHandler(this.btnInstructor_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // RegistrationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnInstructor);
             this.Controls.Add(this.btnStudent);
             this.Name = "RegistrationForm";
@@ -70,6 +81,7 @@
 
         private System.Windows.Forms.Button btnStudent;
         private System.Windows.Forms.Button btnInstructor;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ BusinessProject/Section.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessProject
+{
+    class Section
+    {
+        //Feild
+        private string _CRN;
+        private string _CourseID;
+        private string _TimeDays;
+        private string _RoomNo;
+        private int _Instructor;
+
+          //Constructor
+        public Section(string crn, string courseID, string timeDays, string RoomNo , int instructor) 
+        {
+            _CRN = crn;
+            _CourseID = courseID;
+            _TimeDays = timeDays;
+            _RoomNo = RoomNo;
+            _Instructor = instructor;
+        }
+        //Make ID property
+        public string CRN
+        {
+            get { return _CRN; }
+            set { _CRN = value; }
+        }
+        //Make ID property
+        public string CourseID
+        {
+            get { return _CourseID; }
+            set { _CourseID = value; }
+        }
+        //Make ID property
+        public string TimeDays
+        {
+            get { return _TimeDays; }
+            set { _TimeDays = value; }
+        }
+        //Make ID property
+        public string RoomNo
+        {
+            get { return _RoomNo; }
+            set { _RoomNo = value; }
+        }
+        //Make ID property
+        public int Instructor
+        {
+            get { return _Instructor; }
+            set { _Instructor = value; }
+        }
+        public void display()
+        {
+            Console.WriteLine("CRN           =" + _CRN);
+            Console.WriteLine("Course ID     =" + _CourseID);
+            Console.WriteLine("TimeDays      =" + _TimeDays);
+            Console.WriteLine("Room Number   =" + _RoomNo);
+            Console.WriteLine("Instructor    =" + _Instructor);
+        }
+    }
+}
